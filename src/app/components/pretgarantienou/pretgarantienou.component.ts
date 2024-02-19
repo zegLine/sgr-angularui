2ed65be2e3815ddfe060d4d7830e96618f8adab2
Eugen Zdrincu <eugen.zdrincu@deutschepost.de>
2024-02-19 14:44:47 +0100

    finished create pg component

--- src/app/components/pretgarantienou/pretgarantienou.component.ts
+++ src/app/components/pretgarantienou/pretgarantienou.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatButton} from "@angular/material/button";
+import {PretgarantieService} from "../../services/api/pretgarantie/pretgarantie.service";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-pretgarantienou',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInput,
+    MatButton,
+    FormsModule
+  ],
   templateUrl: './pretgarantienou.component.html',
   styleUrl: './pretgarantienou.component.css'
 })
 export class PretgarantienouComponent {
+  valoare: number = 0.0;
+  constructor(protected pgService: PretgarantieService) {
+  }
 
+  createPretGarante() {
+    this.pgService.createPretGarantie(this.valoare).subscribe({
+      next: () => {
+        console.log('Pret garantie added');
+      }
+    })
+  }
 }
