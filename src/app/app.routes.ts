874fe840965f94f7ac4410229f5d6eaa6d88eafd
Eugen Zdrincu <eugen.zdrincu@deutschepost.de>
2024-02-22 09:34:42 +0100

    added routes for item, purchase

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,9 @@ import {PretgarantieComponent} from "./components/pretgarantie/pretgarantie.comp
 import {PretgarantienouComponent} from "./components/pretgarantienou/pretgarantienou.component";
 import {StoreComponent} from "./components/store/store.component";
 import {StorenouComponent} from "./components/storenou/storenou.component";
+import {PurchaseComponent} from "./components/purchase/purchase.component";
+import {ItemComponent} from "./components/item/item.component";
+import {ItemnouComponent} from "./components/itemnou/itemnou.component";
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent},
@@ -15,5 +18,8 @@ export const routes: Routes = [
   {path: 'garantie/pret/nou', component: PretgarantienouComponent},
   {path: 'store', component: StoreComponent},
   {path: 'store/nou', component: StorenouComponent},
+  {path: 'purchase', component: PurchaseComponent},
+  {path: 'item', component: ItemComponent},
+  {path: 'item/nou', component: ItemnouComponent},
   {path: '', redirectTo: '/start', pathMatch: "full"}
 ];
