7f1a9436b386b969e1ab87f8f44ce87d50dba2dd
Eugen Zdrincu <eugen.zdrincu@deutschepost.de>
2024-02-22 09:34:31 +0100

    added backbone for purchase

---
+++ src/app/components/purchase/purchase.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-purchase',
+  standalone: true,
+  imports: [],
+  templateUrl: './purchase.component.html',
+  styleUrl: './purchase.component.css'
+})
+export class PurchaseComponent {
+
+}
